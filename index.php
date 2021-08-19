e82a29a30a68355c7b4065c33e75e928fbf28ecd
tlubitz <timo.lubitz@gmail.com>
2021-08-19 13:05:09 +0200

    favicon

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="Vestamatic - The global solution provider in dynamic facades">
-    <link rel="shortcut icon" href="img/logo.svg">
+    <link rel="shortcut icon" href="img/favicon.png">
 
     <!-- social media meta data -->
     <meta property="og:url" content="https://www.vestamatic.com" />
